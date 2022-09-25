488110833c127d7d2ffadfe36095452b3070c381
Azima Sati <106227073+sati224@users.noreply.github.com>
2022-09-25 20:27:56 +0400

    modeified App.js

--- src/App.js
+++ src/App.js
@@ -1,336 +1,144 @@
-/*
-=============== 
-Fonts
-===============
-*/
-@import url("https://fonts.googleapis.com/css?family=Open+Sans|Roboto:400,700&display=swap");
+import React, { Component } from "react";
+import axios from "axios";
+import "./styles.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Categories from "./components/Categories";
+import MainMenu from "./components/MainMenu";
+import SearchInput from "./components/SearchInput";
+import CartOffCanvas from "./components/CartOffCanvas";
+import RangeSlider from "./components/RangeSlider";
 
-/*
-=============== 
-Variables
-===============
-*/
-
-:root {
-  /* dark shades of primary color*/
-  --clr-primary-1: hsl(205, 86%, 17%);
-  --clr-primary-2: hsl(205, 77%, 27%);
-  --clr-primary-3: hsl(205, 72%, 37%);
-  --clr-primary-4: hsl(205, 63%, 48%);
-  /* primary/main color */
-  --clr-primary-5: #49a6e9;
-  /* lighter shades of primary color */
-  --clr-primary-6: hsl(205, 89%, 70%);
-  --clr-primary-7: hsl(205, 90%, 76%);
-  --clr-primary-8: hsl(205, 86%, 81%);
-  --clr-primary-9: hsl(205, 90%, 88%);
-  --clr-primary-10: hsl(205, 100%, 96%);
-  /* darkest grey - used for headings */
-  --clr-grey-1: hsl(209, 61%, 16%);
-  --clr-grey-2: hsl(211, 39%, 23%);
-  --clr-grey-3: hsl(209, 34%, 30%);
-  --clr-grey-4: hsl(209, 28%, 39%);
-  /* grey used for paragraphs */
-  --clr-grey-5: hsl(210, 22%, 49%);
-  --clr-grey-6: hsl(209, 23%, 60%);
-  --clr-grey-7: hsl(211, 27%, 70%);
-  --clr-grey-8: hsl(210, 31%, 80%);
-  --clr-grey-9: hsl(212, 33%, 89%);
-  --clr-grey-10: hsl(210, 36%, 96%);
-  --clr-white: #fff;
-  --clr-red-dark: hsl(360, 67%, 44%);
-  --clr-red-light: hsl(360, 71%, 66%);
-  --clr-green-dark: hsl(125, 67%, 44%);
-  --clr-green-light: hsl(125, 71%, 66%);
-  --clr-gold: #c59d5f;
-  --clr-black: #222;
-  --ff-primary: "Roboto", sans-serif;
-  --ff-secondary: "Open Sans", sans-serif;
-  --transition: all 0.3s linear;
-  --spacing: 0.25rem;
-  --radius: 0.5rem;
-  --light-shadow: 0 5px 15px rgba(0, 0, 0, 0.1);
-  --dark-shadow: 0 5px 15px rgba(0, 0, 0, 0.2);
-  --max-width: 1170px;
-  --fixed-width: 620px;
-}
-/*
-=============== 
-Global Styles
-===============
-*/
-
-*,
-::after,
-::before {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-}
-body {
-  font-family: var(--ff-secondary);
-  background: var(--clr-grey-10);
-  color: var(--clr-grey-1);
-  line-height: 1.5;
-  font-size: 0.875rem;
-}
-ul {
-  list-style-type: none;
-}
-a {
-  text-decoration: none;
-}
-img:not(.logo) {
-  width: 100%;
-}
-img {
-  display: block;
-}
-
-h1,
-h2,
-h3,
-h4 {
-  letter-spacing: var(--spacing);
-  text-transform: capitalize;
-  line-height: 1.25;
-  margin-bottom: 0.75rem;
-  font-family: var(--ff-primary);
-}
-h1 {
-  font-size: 3rem;
-}
-h2 {
-  font-size: 2rem;
-}
-h3 {
-  font-size: 1.25rem;
-}
-h4 {
-  font-size: 0.875rem;
-}
-p {
-  margin-bottom: 1.25rem;
-  color: var(--clr-grey-5);
-}
-/* border focus nedds to be updated*/
-
-.input-search {
-  width: 50%;
-  border-color: var(--clr-gold);
-  border-radius: var(--radius);
-  color: var(--clr-gold);
-  padding: 5px;
-  margin: 40px auto;
-  display: block;
-}
-
-@media screen and (min-width: 800px) {
-  h1 {
-    font-size: 4rem;
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      data: [],
+      filteredData: [],
+      categories: [],
+      inputValue: "",
+      cartBusket: [],
+      value: [0, 40] // we stopped here, slider
+      // has a loading problem, need to deal with filter functionality
+    };
+    //this.filterPrice = this.filterPrice.bind(this);
   }
-  h2 {
-    font-size: 2.5rem;
+
+  componentDidMount() {
+    const endPoint =
+      "https://gist.githubusercontent.com/maratgaip/44060c688fcf5f2b7b3985a6d15fdb1d/raw/e93c3dce0826d08c8c6e779cb5e6d9512c8fdced/restaurant-menu.json";
+    axios
+      .get(endPoint)
+      .then((items) => {
+        const categoryData = ["all"];
+        items.data.forEach((item) => {
+          if (!categoryData.includes(item.category)) {
+            categoryData.push(item.category);
+          }
+        });
+        this.setState({
+          data: items.data,
+          filteredData: items.data,
+          categories: categoryData
+        });
+      })
+
+      .catch((error) => "The error is" + error);
+    const basketItems = JSON.parse(sessionStorage.getItem("card"));
+    if (basketItems) {
+      this.setState({ cartBusket: basketItems });
+    }
   }
-  h3 {
-    font-size: 1.75rem;
-  }
-  h4 {
-    font-size: 1rem;
-  }
-  body {
-    font-size: 1rem;
-  }
-  h1,
-  h2,
-  h3,
-  h4 {
-    line-height: 1;
-  }
-}
-/*  global classes */
 
-.btn {
-  text-transform: uppercase;
-  background: transparent;
-  color: var(--clr-black);
-  padding: 0.375rem 0.75rem;
-  letter-spacing: var(--spacing);
-  display: inline-block;
-  transition: var(--transition);
-  font-size: 0.875rem;
-  border: 2px solid var(--clr-black);
-  cursor: pointer;
-  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.2);
-  border-radius: var(--radius);
-  text-align: center;
-}
-.btn:hover {
-  color: var(--clr-white);
-  background: var(--clr-black);
-}
-/* section */
-.section {
-  padding: 5rem 0;
-}
+  handlerBtn = (e) => {
+    const copyData = [...this.state.data];
+    const name = e.target.name;
+    if (name === "all") {
+      this.setState({ filteredData: copyData });
+    } else {
+      const categoryData = copyData.filter((item) => item.category === name);
+      this.setState({ filteredData: categoryData });
+    }
+  };
 
-main {
-  min-height: 100vh;
-  display: grid;
-  place-items: center;
-}
-/*
-=============== 
-Menu
-===============
-*/
+  handlerInput = (e) => {
+    const nameOfMeal = e.target.value;
+    const allMeals = [...this.state.data];
+    const filtered = allMeals.filter((item) =>
+      item.title.toLowerCase().includes(nameOfMeal.toLowerCase())
+    );
+    this.setState({ inputValue: nameOfMeal, filteredData: filtered });
+  };
 
-.menu {
-  padding: 5rem 0;
-}
-.title {
-  text-align: center;
-  margin-bottom: 2rem;
-}
+  addToCart = (index) => {
+    const { cartBusket, filteredData } = this.state;
+    cartBusket.push(filteredData[index]);
+    this.setState({ cartBusket: cartBusket });
+    this.setSessionStorage(cartBusket);
+  };
+  deleteFromCart = (index) => {
+    const { cartBusket } = this.state;
+    cartBusket.splice(index, 1);
+    this.setState({ cartBusket: cartBusket });
+    this.setSessionStorage(cartBusket);
+  };
+  handleChangeSlider = (event) => {
+    // setValue(newValue);
+    //const { valueSlider } = this.state;
+    const newValue = event.target.value;
+    //console.log(newValue);
 
-.underline {
-  width: 5rem;
-  height: 0.25rem;
-  background: var(--clr-gold);
-  margin-left: auto;
-  margin-right: auto;
-}
+    this.setState({ value: newValue });
+    this.filterPrice();
+  };
+  filterPrice = () => {
+    const { value, data, filteredData } = this.state;
+    const priceArray = value;
+    const [lowPrice, highPrice] = priceArray;
+    const priceFilter = data.filter(
+      (item) => lowPrice < item.price && highPrice > item.price
+    );
 
-.cart-underline {
-  width: 90rem;
-  height: 0.25rem;
-  background: var(--clr-gold);
-  margin-left: -70px;
-  margin-right: 150;
-  margin-top: 40px;
-}
+    this.setState({ filteredData: priceFilter });
+  };
 
-.btn-container {
-  margin-bottom: 4rem;
-  display: flex;
-  justify-content: center;
-}
+  setSessionStorage = (favMeals) => {
+    sessionStorage.setItem("card", JSON.stringify(favMeals));
+  };
 
-.filter-btn,
-.filter-btncart {
-  background: transparent;
-  border-color: var(--clr-gold);
-  font-size: 1rem;
-  text-transform: capitalize;
-  margin: 0 0.5rem;
-  letter-spacing: 1px;
-  border-radius: var(--radius);
-  padding: 0.375rem 0.75rem;
-  color: var(--clr-gold);
-  cursor: pointer;
-  transition: var(--transition);
-}
-.filter-btn:hover {
-  background: var(--clr-gold);
-  color: var(--clr-white);
-}
+  render() {
+    const {
+      categories,
+      filteredData,
+      inputValue,
+      cartBusket,
+      value
+    } = this.state;
+    return (
+      <div className="App">
+        <div className="title">
+          <h2>Our Menu</h2>
+          <div className="underline"></div>
+        </div>
+        <Categories categoriesBtn={categories} handlerBtn={this.handlerBtn} />
+        <SearchInput value={inputValue} handlerInput={this.handlerInput} />
 
-.filter-btncart:hover {
-  background: var(--clr-gold);
-  color: var(--clr-white);
-}
+        <RangeSlider
+          value={value}
+          handleChangeSlider={this.handleChangeSlider}
+        />
 
-.filter-btncart {
-  position: absolute;
-  top: 5px;
-  right: 0;
-}
-.css-1v5z18m {
-  width: 50% !important;
-  margin: 1rem auto;
-}
-.css-187mznn-MuiSlider-root {
-  color: var(--clr-gold) !important;
-}
-.section-center {
-  width: 90vw;
-  margin: 0 auto;
-  max-width: 1170px;
-  display: grid;
-  gap: 3rem 2rem;
-  justify-items: center;
-}
-.menu-item {
-  display: grid;
-  gap: 1rem 2rem;
-  max-width: 25rem;
-}
-.photo,
-.cart-photo {
-  object-fit: cover;
-  height: 200px;
-  border: 0.25rem solid var(--clr-gold);
-  border-radius: var(--radius);
-}
-
-.cart-photo {
-  height: 100px;
-}
-
-.item-info header {
-  display: flex;
-  justify-content: space-between;
-  border-bottom: 0.5px dotted var(--clr-grey-5);
-}
-.item-info h4 {
-  margin-bottom: 0.5rem;
-}
-.price {
-  color: var(--clr-gold);
-  padding: 5px;
-}
-
-.delete {
-  width: 20%;
-  cursor: pointer;
-  color: white;
-  letter-spacing: 0.1em;
-  background-color: red;
-  border: none;
-  border-radius: 5px;
-  margin-bottom: 15px;
-}
-
-.delete:active {
-  transform: translateY(4px);
-}
-
-.item-text {
-  margin-bottom: 0;
-  padding-top: 1rem;
-}
-
-@media screen and (min-width: 768px) {
-  .menu-item {
-    grid-template-columns: 225px 1fr;
-    gap: 0 1.25rem;
-    max-width: 40rem;
-  }
-  .photo {
-    height: 175px;
-  }
-}
-@media screen and (min-width: 1200px) {
-  .section-center {
-    width: 95vw;
-    grid-template-columns: 1fr 1fr;
-  }
-  .photo {
-    height: 150px;
+        <CartOffCanvas
+          cartBusket={cartBusket}
+          deleteFromCart={this.deleteFromCart}
+        />
+        {filteredData.length > 0 ? (
+          <MainMenu addToCart={this.addToCart} getData={filteredData} />
+        ) : (
+          <h3 style={{ textAlign: "center", color: "red" }}>not found</h3>
+        )}
+      </div>
+    );
   }
 }
 
-.search-slider-wrapper {
-  width: 40%;
-  margin: auto;
-}
+export default App;
